2828c8e63427c586ffd0b4687142aa5f5520f9e4
Yannick Bals <yannick.bals@web.de>
2018-06-01 09:25:11 +0200

    Init project

---
+++ app/src/main/java/de/thm/toolbarexample/MainActivity.java
@@ -0,0 +1,10 @@
+package de.thm.toolbarexample;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yannick Bals on 01.06.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
